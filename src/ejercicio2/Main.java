c00dc309f22cb436320f715741b674e42701f3ca
Romina Alaca <alacaromina@gmail.com>
2025-09-07 18:32:53 -0300

    Realizado hasta el ejercicio 4

--- src/ejercicio2/Main.java
+++ src/ejercicio2/Main.java
@@ -1,5 +1,19 @@
-package Ejercicio2;
+package ejercicio2;
+
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Ingrese un numero entero: ");
+        int numero=scanner.nextInt();
+
+        if (numero%2==0) {
+           System.out.print("El numero es impar.El doble del numero es:"+(numero*2));
+        } else {System.out.print("El numero es par.El triple del numero es:"+(numero*3));}
+        
+        scanner.close();
+    }
 
 }
